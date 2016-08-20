b4b0a1a1cc0c97885094c99cca241236cde1f3c6
payshangjj <yulei024@163.com>
2016-08-20 16:19:25 +0800

    add shadowsocks for ubuntu

--- Git/git.sh
+++ Git/git.sh
@@ -1,3 +1,5 @@
 git remote add origin https://github.com/payshangjj/code-repositories.git
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
 git push -u origin master	#push code to GitHup
 
